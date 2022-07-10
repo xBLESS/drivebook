8957d7c3b00a286d3d0e60774f2486ac23d818df
Tim Weyerbrock <weyerbrock@outlook.de>
2022-07-10 23:21:16 +0200

    Added decorations to TextFields

--- lib/screens/loginscreen/loginscreen.dart
+++ lib/screens/loginscreen/loginscreen.dart
@@ -19,24 +19,36 @@ class LoginScreen extends StatelessWidget {
     final userProvider = Provider.of<AppwriteClient>(context);
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(flex: 3),
-            TextField(
-              controller: _tecUsername,
-            ),
-            TextField(
-              controller: _tecPassword,
-            ),
-            const Spacer(flex: 2),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(flex: 3),
+              TextField(
+                controller: _tecUsername,
+                decoration: const InputDecoration(
+                  label: Text('Username'),
+                ),
+              ),
+              TextField(
+                autocorrect: false,
+                controller: _tecPassword,
+                decoration: const InputDecoration(
+                  label: Text('Password'),
+                ),
+                enableSuggestions: false,
+                obscureText: true,
+              ),
+              const Spacer(flex: 2),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           userProvider.loginUser(_tecUsername.text, _tecPassword.text);
-          Navigator.pushNamed(context, MyHomePage.routename);
+          // ToDo Der benutzer soll nicht vom Hauptbildschirm zum LoginScreen zurückkehren sollen
+          Navigator.popAndPushNamed(context, MyHomePage.routename);
         },
         label: const Text('Login'),
       ),
