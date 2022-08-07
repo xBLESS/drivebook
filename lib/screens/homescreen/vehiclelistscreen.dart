a94b0d4c6c13a8fa8c892a361117cbe798b206bc
EDEN <weyerbrock@outlook.de>
2022-08-07 20:28:13 +0200

    Commented

--- lib/screens/homescreen/vehiclelistscreen.dart
+++ lib/screens/homescreen/vehiclelistscreen.dart
@@ -22,8 +22,10 @@ class VehicleListScreen extends StatelessWidget {
         mileage: Value(123456),
         buildDate: Value(DateTime(1987, 1, 1, 1, 1)),
         licensePlate: Value('VIE TW 69'),
+        // Fuel
         primaryFuelCapacity: Value(50),
         primaryFuelTypeId: Value(0),
+        // Buy
         buyDateTime: Value(DateTime(2021, 1, 2, 0, 0, 0)),
         buyMileage: Value(70500),
         buyPrice: Value(0),
