24311c51e47e1459f777b29be5b61c1033627a3f
EDEN <weyerbrock@outlook.de>
2022-08-14 00:36:56 +0200

    Extension hinzugeügt um Color aufzuhellen

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -1,5 +1,15 @@
+import 'package:flutter/material.dart';
+
 extension BoolParsing on String {
   bool parseBool() {
     return this.toLowerCase() == 'true';
+  }
+}
+
+extension ColorExtension on Color {
+  Color lightenColor() {
+    final hsl = HSLColor.fromColor(this);
+    final lighter = hsl.withLightness((hsl.lightness + 0.2).clamp(0.0, 1.0));
+    return lighter.toColor();
   }
 }
